1c440741d0242e0b82b0c3fd1ca8f6f88ea926d4
kl4u5 <kl4u5.j3n53n@gmail.com>
2017-06-01 12:07:40 +0200

    Baseline project

---
+++ TestingGitVersion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGitVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Here ar some danish characters: æøå ÆØÅ");
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+          
+            string version = fvi.FileVersion;
+
+            Console.WriteLine("FileVersion: " + version);
+
+            Console.Write("Press any key to exit:");
+            Console.ReadKey();
+        }
+    }
+}

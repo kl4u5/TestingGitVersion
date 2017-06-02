e32ea008f4aa2b6c7d5e6356ce6a9d42f46d0814
kl4u5 <kl4u5.j3n53n@gmail.com>
2017-06-02 14:29:01 +0200

    Commit to master

--- TestingGitVersion/Program.cs
+++ TestingGitVersion/Program.cs
@@ -12,7 +12,8 @@ namespace TestingGitVersion
         static void Main(string[] args)
         {
             Console.WriteLine("Hello. Here ar some danish characters: æøå HEJ");
-            Console.WriteLine("Hotfix 1 done.");    
+            Console.WriteLine("Hotfix 1 done.");
+            Console.WriteLine("Commit to master done.");    
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
           
